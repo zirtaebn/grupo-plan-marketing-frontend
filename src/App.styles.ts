aed7e2ef88da8a65fcae2c4b4004fbb404c7e346
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-27 20:49:08 -0300

    add App styles

---
+++ src/App.styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+// utilities
+import Colors from './theme/colors'
+
+export const AppContainer = styled.div`
+
+    background-color:${Colors.background.darkGray};
+    height:100vh;
+
+    ul {
+
+        color:#fff
+    }
+
+
+`
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css';
+// styles
+import { AppContainer } from './App.styles'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <AppContainer>
+
+      <nav>
+        <img src="" alt="plan marketing logo" />
+      </nav>
+
+      <ul>
+        <li>
+          <div>01.</div>
+          <p>/QUEM SOMOS</p>
+        </li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
      
-    </div>
+    </AppContainer>
   );
 }
 
