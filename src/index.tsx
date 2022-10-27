0135819f9e5545fe2033020648fde09d0986acc4
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-27 20:48:56 -0300

    add index styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+// styles
+import GlobalStyle from './index.styles'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <GlobalStyle />
   </React.StrictMode>
 );
 
