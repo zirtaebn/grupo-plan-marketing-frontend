8c6c9a8885b2cb1758aba41107d17ced324ef058
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-28 11:50:41 -0300

    add AppContent div

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 // components
 import Menu from './components/Menu'
+import WhatWeDo from './components/WhatWeDo'
 
 // styles
-import { AppContainer } from './App.styles'
+import { AppContainer, AppContent } from './App.styles'
 
 const App = () => {
   return (
     <AppContainer>
 
       <Menu />
+
+      <AppContent>
+        <WhatWeDo />
+      </AppContent>
 
       
      
