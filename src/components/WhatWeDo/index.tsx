22ad10c1fa60bbf122a8e0459a4924e733687c8d
Beatriz Sant'Anna <zirtaebn@gmail.com>
2022-10-28 11:50:19 -0300

    add WhatWeDo content

--- src/components/WhatWeDo/index.tsx
+++ src/components/WhatWeDo/index.tsx
@@ -1,7 +1,14 @@
+import { BsController, BsArrowDownRight } from 'react-icons/bs'
+
 // styles
 import SectionTitle from '../SectionTitle'
 import {
-    WhatWeDoContainer,
+    WhatWeDoContainer, 
+    WhatWeDoLeft,
+    WhatWeDoRight,
+    WhatWeDoContent,
+    WhatWeDoImage,
+    WhatWeDoDescription
     
    
 } from './styles'
@@ -11,14 +18,67 @@ const WhatWeDo = () => {
     return (
 
         <WhatWeDoContainer>
-            <SectionTitle>
-                <div>02.</div>
-                <p>/O que fazemos</p>
-                <h2>/Games</h2>
-            </SectionTitle>
-
+            <WhatWeDoLeft>
+                <SectionTitle>
+                    <div>02.</div>
+                    <h3>/O que fazemos</h3>
+                    <h2>/Games</h2>
+                </SectionTitle>
             
 
+                <h4>Desenvolvimento de games digitais.</h4>
+                <h5>Empresa especialista em desenvolvimento games digitais e gamificação.</h5>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </WhatWeDoLeft>
+
+            <WhatWeDoRight>
+                <WhatWeDoContent>
+                    <WhatWeDoImage 
+                        src='https://t.tudocdn.net/503907'
+                        alt='garoto jogando no celular'
+                    />
+
+                    <WhatWeDoDescription>
+                        A <span>PlanXp</span> tem uma equipe qualificada para o <span>desenvolvimento de jogos digitais</span> para <span>dispositivos móveis</span> (smartphones, tablets e afins).
+
+                    </WhatWeDoDescription>
+                </WhatWeDoContent>
+                <WhatWeDoContent>
+                    <WhatWeDoImage 
+                        src='https://media.istockphoto.com/photos/men-playing-video-games-picture-id1132281737?k=20&m=1132281737&s=170667a&w=0&h=Vv5u6xQFeFzixD1npl5mENRY5MA1yoqt_-ILct0kWSI='
+                        alt='garoto jogando no computador'
+                    />
+
+                    <WhatWeDoDescription>
+                        
+                        Desenvolver games para anunciar seus produtos e serviços, através da <span>gamificação</span> (aplicações que utilizam dinâmicas de jogos para engajamento de usuários, resolução de problemas e melhoria no aprendizado).
+                    </WhatWeDoDescription>
+                </WhatWeDoContent>
+                <WhatWeDoContent>
+                    <WhatWeDoImage 
+                        src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCEyssQdH3dTg7bi2EDKeWseOxSLpFisbkrg&usqp=CAU'
+                        alt='homem jogando no celular'
+                    />
+
+                    <WhatWeDoDescription>
+                        Utilizando as últimas tecnologias para a <span>criação de games</span>, trabalhamos de acordo com as necessidades dos clientes, sempre buscando oferecer o resultlado que ele busca.
+                    </WhatWeDoDescription>
+                </WhatWeDoContent>
+                <WhatWeDoContent>
+                    <div style={{width:'135px', height:'120px', display:'flex',alignItems:'flex-end'}}>
+                        <BsController size={35}/>
+                    </div>
+
+                    <WhatWeDoDescription>Experimente inovar com a <span>PlanXP</span> para <span>desenvolver os seus jogos digitais!</span></WhatWeDoDescription>
+
+                    <a href="">
+                        ENTRE EM CONTATO AGORA MESMO
+                        <BsArrowDownRight size={10} />
+                        
+                    </a>
+                </WhatWeDoContent>
+
+            </WhatWeDoRight>
         </WhatWeDoContainer>
         
     )
--- src/components/WhatWeDo/styles.ts
+++ src/components/WhatWeDo/styles.ts
@@ -1,9 +1,104 @@
 import styled from 'styled-components'
+import Colors from '../../theme/colors'
 
 export const WhatWeDoContainer = styled.section`
 
     display:flex;
-    align-items:center;
+    align-items: center;
+    justify-content:center;
     height:100vh;
+`
 
+export const WhatWeDoLeft = styled.div`
+
+    display:flex;
+    flex-direction:column;
+    height:400px;
+
+    h4, h5 {
+
+        color:${Colors.text.lightGray}
+    }
+
+    h4 {
+
+        font-size:18px;
+    }
+
+    h5 {
+        margin:20px 0;
+        font-size:13px;
+    }
+
+    p {
+        color:${Colors.text.white};
+        font-size:12px;
+        max-width:450px;
+        line-height:20px;
+    }
+`
+
+export const WhatWeDoRight = styled.div`
+
+    display:flex;
+    margin-left:50px;
+    margin-right:20px;
+`
+export const WhatWeDoContent = styled.div`
+
+    margin:0 5px;
+    width:135px;
+
+    svg {
+
+        color:${Colors.text.white};
+    }
+
+    &:last-child {
+
+        p {
+
+            font-size:20px;
+            line-height:30px;
+        }
+
+        a {
+
+            text-decoration:none;
+            color:${Colors.text.lightGray};
+            font-size:12px;
+
+            svg {
+
+                margin-left:5px;
+            }
+
+            &:hover {
+
+                opacity:.5;
+            }
+        }
+    }
+`
+
+export const WhatWeDoImage = styled.img`
+
+    width:100%;
+    height:125px;
+`
+
+export const WhatWeDoDescription = styled.p`
+
+    color:${Colors.text.white};
+    font-weight:300;
+    font-size:12px;
+    max-width:135px;
+    line-height:22px;
+    margin-top:5px;
+    margin-bottom:15px;
+
+    span {
+
+        font-weight:bold;
+    }
 `
